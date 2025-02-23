dd87bfdaa7e1046cb36b5a96d8cb6e7fafa89f98
Tacoman99 <jorgepadres789@gmail.com>
2025-02-22 21:43:47 -0800

    finished technical indicators

--- services/technical_indicators/run.py
+++ services/technical_indicators/run.py
@@ -1,64 +1,71 @@
+from candle import update_candles
 from loguru import logger
 from quixstreams import Application
+from technical_indicators import compute_indicators
 
 
 def main(
     kafka_broker_address: str,
-    kafka_consumer_group: str,
     kafka_input_topic: str,
     kafka_output_topic: str,
-    num_candles: int,
+    kafka_consumer_group: str,
+    max_candles_in_state: int,
 ):
     """
     3 steps:
-    1. Create a Kafka consumer to read the candles from the input topic.
-    2. Create a Kafka producer to write the technical indicators to the output topic.
-    3. Create a technical indicators calculator.
+    1. Ingests candles from the kafka input topic
+    2. Computes technical indicators
+    3. Sends the technical indicators to the kafka output topic
 
     Args:
-        kafka_broker_address (str): The address of the Kafka broker.
-        kafka_consumer_group (str): The consumer group for the Kafka consumer.
-        kafka_input_topic (str): The input topic for the Kafka consumer.
-        kafka_output_topic (str): The output topic for the Kafka producer.
-        num_candles (int): The number of candles to use for the technical indicators.
+        kafka_broker_address: The address of the kafka broker
+        kafka_input_topic: The topic to ingest candles from
+        kafka_output_topic: The topic to send technical indicators to
+        kafka_consumer_group: The consumer group for the kafka consumer
+        max_candles_in_state: The maximum number of candles to keep in the state
+    Returns:
+        None
     """
-    print('Hello from technical-indicators!')
+    logger.info('Hello from technical-indicators!')
 
     app = Application(
         broker_address=kafka_broker_address,
         consumer_group=kafka_consumer_group,
     )
 
+    # Define the input and output topics of our streaming application
     input_topic = app.topic(
         name=kafka_input_topic,
         value_deserializer='json',
     )
-
     output_topic = app.topic(
         name=kafka_output_topic,
         value_serializer='json',
     )
+    # Create a Streaming DataFrame so we can start transforming data in real time
+    sdf = app.dataframe(topic=input_topic)
 
-    sdf = app.dataframe(
-        topic=input_topic,
-    )
+    # Update the list of candles in the state
+    sdf = sdf.apply(update_candles, stateful=True)
 
-    sdf = sdf.update(lambda value: logger.info(f'Candle: {value}'))
+    # Compute the technical indicators from the candles in the state
+    sdf = sdf.apply(compute_indicators, stateful=True)
 
-    sdf = sdf.to_topic(
-        topic=output_topic,
-    )
+    sdf = sdf.update(lambda value: logger.debug(f'Final message: {value}'))
 
-    app.run(sdf)
+    # Send the final messages to the output topic
+    sdf = sdf.to_topic(output_topic)
+
+    app.run()
 
 
 if __name__ == '__main__':
     from config import config
 
     main(
-        kafka_broker_address=config.KAFKA_BROKER_ADDRESS,
-        kafka_consumer_group=config.KAFKA_CONSUMER_GROUP,
-        kafka_input_topic=config.KAFKA_INPUT_TOPIC,
-        kafka_output_topic=config.KAFKA_OUTPUT_TOPIC,
-        num_candles=config.NUM_CANDLES,
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_input_topic=config.kafka_input_topic,
+        kafka_output_topic=config.kafka_output_topic,
+        kafka_consumer_group=config.kafka_consumer_group,
+        max_candles_in_state=config.max_candles_in_state,
     )
